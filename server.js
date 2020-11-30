12957761f2f2195c1da6bc1840916ebf4fd10347
jqha <70602851+jqha@users.noreply.github.com>
2020-11-30 15:33:22 -0500

    Test

---
+++ server.js
@@ -0,0 +1,33 @@
+// These are our required libraries to make the server work.
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.route('/api')
+  .get(async (req, res) => {
+    console.log('GET request detected');
+    console.log('Fetch request data', json);
+  })
+  .post(async (req, res) => {
+    console.log('POST request detected');
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
